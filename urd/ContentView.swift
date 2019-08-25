914e9304eeed6971095903628f36089cb7dbac1b
olaven <olavsundfoer@gmail.com>
2019-08-25 15:37:16 +0200

    can ask for permissions to access eventstore

--- urd/ContentView.swift
+++ urd/ContentView.swift
@@ -7,15 +7,69 @@
 //
 
 import SwiftUI
+import EventKit
+
+
+func askForCalendarPermission() {
+    
+    
+    
+}
 
 struct ContentView : View {
+    
+    @State private var hasAccess = EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized;
+    let eventStore = EKEventStore()
+    
+    
     var body: some View {
-        Text("Hello World")
         VStack {
-            Text("Hei igjen")
+            if (hasAccess) {
+                
+                Text("You have access to calendar")
+            } else {
+                
+                Button(
+                    action: {
+                        
+                        switch EKEventStore.authorizationStatus(for: .event) {
+                            
+                        case .authorized:
+                            print("Authorized")
+                            
+                        case .notDetermined:
+                            self.eventStore.requestAccess(to: .event, completion: {
+                                (granted: Bool, error: Error?) -> Void in
+                                
+                                if granted {
+                                    print("Granted")
+                                } else {
+                                    print("Denied")
+                                }
+                            })
+                        case .denied:
+                            
+                            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                                return
+                            }
+                            
+                            if UIApplication.shared.canOpenURL(settingsUrl) {
+                                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                                    print("Settings opened: \(success)") // Prints true
+                                })
+                            }
+                        default:
+                            print("Default case")
+                        }
+                    }) {
+                        
+                        Text("Give access")
+                    }
+            }
         }
     }
 }
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
---
+++ urd/models/Calendar.swift
@@ -0,0 +1,13 @@
+//
+//  Calendar.swift
+//  urd
+//
+//  Created by Olav on 18/08/2019.
+//  Copyright © 2019 olaven. All rights reserved.
+//
+
+import Foundation
+
+struct Calendar {
+    
+}
